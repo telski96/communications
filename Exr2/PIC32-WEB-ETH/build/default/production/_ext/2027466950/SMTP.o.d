058233a979a270a36b3ca7f33db7dfda8d82b63d
nikolovd <dnikolov@elsys-bg.org>
2018-03-21 12:33:32 +0200

    add pic32web-eth

---
+++ Exr2/PIC32-WEB-ETH/build/default/production/_ext/2027466950/SMTP.o.d
@@ -0,0 +1,38 @@
+build/default/production/_ext/2027466950/SMTP.o:  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/TCPIP\ Stack/SMTP.c  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/TCPIP/Demo\ App/TCPIPConfig.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/TCPIP/Demo\ App/Configs/TCPIP\ OLIMEX_PIC32-WEB.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/GenericTypeDefs.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/Compiler.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/TCPIP.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/TCPIP/Demo\ App/HardwareProfile.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/TCPIP/Demo\ App/Configs/HWP\ OLIMEX_PIC32-WEB.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/StackTsk.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/Helpers.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/Delay.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/Tick.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/MAC.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/ENCX24J600.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/IP.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/ARP.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/AutoIP.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/Hashes.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/XEEPROM.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/SPIFlash.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/SPIRAM.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/UDP.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/TCP.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/BerkeleyAPI.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/UART.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/DHCP.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/DNS.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/MPFS2.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/HTTP2.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/ICMP.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/Announce.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/NBNS.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/Telnet.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/SMTP.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/Reboot.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/SNTP.h  \
+ ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/TCPIP\ Stack/TCPPerformanceTest.h 
