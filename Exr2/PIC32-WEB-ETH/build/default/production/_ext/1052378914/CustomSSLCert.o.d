088b9df71336571f71b9814757c6006847e6e9db
nikolovd <dnikolov@elsys-bg.org>
2018-03-21 12:37:15 +0200

    erase commit

--- Exr2/PIC32-WEB-ETH/build/default/production/_ext/1052378914/CustomSSLCert.o.d
+++
@@ -1,6 +0,0 @@
-build/default/production/_ext/1052378914/CustomSSLCert.o:  \
- ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/TCPIP/Demo\ App/CustomSSLCert.c  \
- ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/TCPIP/Demo\ App/TCPIPConfig.h  \
- ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/TCPIP/Demo\ App/Configs/TCPIP\ OLIMEX_PIC32-WEB.h  \
- ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/GenericTypeDefs.h  \
- ../../Desktop/Current/Comm-FETT/PIC32-WEB_5.42/PIC32-WEB\ 5.42/Microchip/Include/Compiler.h 
